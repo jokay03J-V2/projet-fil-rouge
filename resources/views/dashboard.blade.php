0adcf222cb8749cd5af10f7e6d416756bcf361b4
Cyberfox206 <bbrontesi@edenschool.fr>
2024-06-03 16:39:19 +0200

    update layout for home page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,14 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <h2>Accueil</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, quam quia illo animi voluptate doloribus.
+            Ut vero neque molestias impedit, assumenda, excepturi blanditiis veritatis reiciendis expedita libero
+            architecto mollitia saepe.
+        </p>
     </x-slot>
 
-    <div class="py-12">
+    <!-- <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
@@ -13,5 +16,5 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 </x-app-layout>
