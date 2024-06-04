69094b57483fb86ab1a154ddf10ef450553beb27
Cyberfox206 <bbrontesi@edenschool.fr>
2024-06-04 15:15:35 +0200

    update roote for new name of home page

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Home');
 })->name('page.home');
 
 Route::get('/advices', function () {
