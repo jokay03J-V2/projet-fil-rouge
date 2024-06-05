cfc89546c61e3cdec96bd87767bfced8ae624154
jokay03J-V2 <jmagro@edenschool.fr>
2024-06-05 12:00:13 +0200

    use correct components

--- resources/views/urgencyNumber/index.blade.php
+++ resources/views/urgencyNumber/index.blade.php
@@ -1,13 +1,14 @@
 <x-app-layout>
     <x-slot name="header">
         <h1>Rechercher un numéro d’urgence</h1>
-        <form action="{{route('page.numbers')}}">
-            <select name="query" id="query" class="text-black">
+        <form action="{{route('page.numbers')}}" class="flex gap-1">
+            <x-select name="query" id="query">
                 <option value="null" disabled selected>Rechercher par catégorie</option>
                 @foreach ($categories as $category)
                     <option value="{{$category->id}}">{{$category->name}}</option>
                 @endforeach
-            </select>
+            </x-select>
+            <x-submit-button>Send</x-submit-button>
         </form>
     </x-slot>
 </x-app-layout>
