bbdd26d8bca5a904eda20403af2efb2fe21aeaf3
Cannellespeculos <cjeanpierre@edenschool.fr>
2024-06-05 12:00:33 +0200

    create InformationPoint factory

---
+++ database/factories/InformationPointFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class InformationPointFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'hourly' => $this->faker->text(),
+//            'departement' => $this->faker->(),
+        ];
+    }
+}
