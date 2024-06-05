5a27b251fcf1ebcec0d957dbefe6db6b16fa3ea5
Ugo <udauvergne@edenschool.fr>
2024-06-05 12:00:24 +0200

    search by title and category

--- resources/views/advices.blade.php
+++ resources/views/advices.blade.php
@@ -1,43 +1,46 @@
 <x-app-layout>
     <x-slot name="header">
-        <h1>Rechercher un conseil</h1>
-        <form action="" class="flex flex-col w-11/12 m-auto">
-            <label for="search">Rechercher un conseil</label>
-            <input type="search" name="search" id="search" class="rounded-lg border-none"
+        <h1 class="text-6xl font-title p-6 text-center">Rechercher un conseil</h1>
+
+        <form action="" method="GET" class="flex flex-col w-11/12 m-auto">
+            @csrf
+            <label for="search" class="text-black">Rechercher un conseil</label>
+            <input type="search" name="query" id="search" class="rounded-lg border-none text-black"
                 placeholder="Rechercher par titre">
-        </form>
-
-        <form action="" class=" w-11/12 m-auto">
-            <select name="categoryAdvice" id="" class="rounded-lg border-none w-80 mt-8">
-                <option value="all" selected>toutes les categories</option>
-                <?php 
-        foreach ($categorys as $key => $value) {
-            ?>
-                <option value="<?php    echo $value->name?>"><?php    echo $value->name; ?></option>
-                <?php
-}
-        ?>
+                
+            <select name="categoryAdvice" class="rounded-lg border-none w-80 m-6 text-black">
+                <option value="all" selected>Toutes les catégories</option>
+                @foreach ($categorys as $category)
+                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                @endforeach
             </select>
-            <button>ok</button>
-
+            <button type="submit" class="text-lg border p-3 rounded-lg">OK</button>
         </form>
     </x-slot>
-    <div class="grid grid-cols-1 gap-8 justify-items-center md:grid-cols-2 lg:grid-cols-3 ">
 
-
-        <?php 
-        foreach ($advices as $key => $value) {
-            ?>
-        <a href="/advices/<?php    echo $value->id;?>" class="w-11/12 ">
-            <article class="flex flex-col rounded-xl border border-blue-600 h-96">
-                <img class="object-cover rounded-t-xl h-3/4 " src="<?php    echo $value->img;?>" alt="nemo">
-                <h2 class="p-7 truncate"><?php    echo $value->name;?></h2>
-            </article>
-        </a>
-        <?php
-}
-        ?>
-
-
+    <div class="grid grid-cols-1 gap-8 justify-items-center md:grid-cols-2 lg:grid-cols-3 mt-20 mb-20">
+        @if(isset($results))
+            @if($results->isEmpty())
+                <h3 class="text-black">Aucun résultat trouvé</h3>
+            @else
+                @foreach ($results as $value)
+                    <a href="/advices/{{ $value->id }}" class="w-11/12 ">
+                        <article class="flex flex-col rounded-xl border border-blue-600 h-96">
+                            <img class="object-cover rounded-t-xl h-3/4 " src="{{ $value->img }}" alt="nemo">
+                            <h2 class="p-7 truncate">{{ $value->name }}</h2>
+                        </article>
+                    </a>
+                @endforeach
+            @endif
+        @else
+            @foreach ($advices as $value)
+                <a href="/advices/{{ $value->id }}" class="w-11/12 ">
+                    <article class="flex flex-col rounded-xl border border-blue-600 h-96">
+                        <img class="object-cover rounded-t-xl h-3/4 " src="{{ $value->img }}" alt="nemo">
+                        <h2 class="p-7 truncate">{{ $value->name }}</h2>
+                    </article>
+                </a>
+            @endforeach
+        @endif
     </div>
 </x-app-layout>
