df227cd1f7b06f7ae38c7a5f028bd7518739460d
jokay03J-V2 <jmagro@edenschool.fr>
2024-06-05 10:13:57 +0200

    add urgency category and urgency number seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\UrgencyCategory;
+use App\Models\UrgencyNumber;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        UrgencyCategory::factory(5)->create()->each(function ($model) {
+            UrgencyNumber::factory(1, ['category_id' => $model->id])->create();
+        });
     }
 }
