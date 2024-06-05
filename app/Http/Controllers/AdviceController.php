063f03e44eae85fcd5dde0c99cf696fb2e411a3c
Ugo <udauvergne@edenschool.fr>
2024-06-05 15:52:37 +0200

    comment

--- app/Http/Controllers/AdviceController.php
+++ app/Http/Controllers/AdviceController.php
@@ -14,7 +14,7 @@ class AdviceController extends Controller
     {
         $query = $request->input('query');
         $categoryId = $request->input('categoryAdvice');
-
+        // retrieves the data sent by the input request and categoryAdvice
         $queryBuilder = Advice::query();
 
         if ($query) {
@@ -24,10 +24,10 @@ class AdviceController extends Controller
         if ($categoryId && $categoryId !== 'all') {
             $queryBuilder->where('category_id', $categoryId);
         }
-
+        // check if the data received by the query input is equal to the data in the name column of our database, check if the value of the categoryAdvice input is equal to all 
         $advices = $queryBuilder->get();
         $categorys = CategoryAdvice::all();
-
+        // return variables on the view advices 
         return view('advices', [
             'advices' => $advices,
             'categorys' => $categorys,
--- app/Models/CategoryAdvice.php
+++ app/Models/CategoryAdvice.php
@@ -12,6 +12,9 @@ class CategoryAdvice extends Model
     public $timestamps = false;
     protected $table = 'category_advice';
 
+     // Declare attributes that can be assigned en masse
+    // This allows you to use the template by directly assigning values to the specified attributes
+    
     protected $fillable = [
         'id',
         'name',
