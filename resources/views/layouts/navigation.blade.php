de6ad9905df17e2faf8db85848b6796c788bb7ae
Cyberfox206 <bbrontesi@edenschool.fr>
2024-06-05 09:33:19 +0200

    add test and correct home page + env.exemple

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,6 @@
-<nav x-data="{ open: false }" class="w-full flex justify-between items-center">
+<nav x-data="{ open: false }" class="w-full flex justify-between items-center p-2">
     <a href="{{route('page.home')}}" class="text-5xl ml-4 font-title">titre</a>
-    <x-lucide-menu class="w-8 lg:hidden"></x-lucide-menu>
+    <x-lucide-menu class="w-14 lg:hidden "></x-lucide-menu>
     <ul class="hidden lg:flex">
         <x-link-nav href="#">conseils</x-link-nav>
         <x-link-nav href="#">Practiciens</x-link-nav>
