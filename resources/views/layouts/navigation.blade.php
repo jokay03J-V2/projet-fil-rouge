216ac92549d065c65c6780b2074f5ae8fff7202f
Cyberfox206 <bbrontesi@edenschool.fr>
2024-06-05 11:56:37 +0200

    correct pr and add a comments

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,5 +1,5 @@
 @Vite(['resources/js/home_menu.js'])
-<nav x-data="{ open: false }" class="w-full flex justify-between items-center p-2 relative">
+<nav class="w-full flex justify-between items-center p-2 relative">
     <a href="{{route('page.home')}}" class="text-5xl ml-4 font-title">titre</a>
     <x-lucide-menu class="w-14 lg:hidden z-10" id="btn_home_menu_action"></x-lucide-menu>
     <ul class="w-0 overflow-hidden pt-14 bg-white text-black h-screen right-0 top-0 absolute lg:flex lg:bg-transparent lg:w-fit lg:h-fit lg:relative lg:text-white lg:pt-0 transition-all ease-in-out duration-700"
