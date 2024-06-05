dc6e3b049b4d7a4f89df2df89b600c08d19a9e15
Cannellespeculos <cjeanpierre@edenschool.fr>
2024-06-05 11:47:54 +0200

    add information point on mcd and mld and correct mcd

---
+++ database/migrations/2024_06_05_090219_information_point.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+//        create table information_service
+        Schema::create('information_service', function (Blueprint $table) {
+            $table->id("id")->primary();
+            $table->string('name');
+        });
+
+//        create table information_point
+        Schema::create('information_point', function (Blueprint $table) {
+            $table->id("id")->primary();
+            $table->string('name');
+            $table->string('hourly');
+            $table->string('departement');
+        });
+
+//        create table information_service-information
+        Schema::create('information_service-information', function (Blueprint $table) {
+            $table->unsignedBigInteger("service_id");
+            $table->foreign('service_id')->references('id')->on('information_service');
+            $table->unsignedBigInteger("information_id");
+            $table->foreign('information_id')->references('id')->on('information_point');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
