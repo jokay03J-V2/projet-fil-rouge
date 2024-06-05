bf0a6fb776a3bd84eca2a53f5c22813226d798cb
jokay03J-V2 <jmagro@edenschool.fr>
2024-06-05 10:12:50 +0200

    Add UrgencyNumber and UrgencyCategory models

---
+++ app/Models/UrgencyNumber.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UrgencyNumber extends Model
+{
+    protected $table = "emergency_number";
+    public $timestamps = false;
+    use HasFactory;
+
+    protected $fillable = ["category_id", "content"];
+}
