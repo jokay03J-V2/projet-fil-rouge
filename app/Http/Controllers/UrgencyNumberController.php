290f8ce79afda12393009d2418a3154d991f4a2e
jokay03J-V2 <jmagro@edenschool.fr>
2024-06-06 14:20:36 +0200

    Add search and display urgency numbers

--- app/Http/Controllers/UrgencyNumberController.php
+++ app/Http/Controllers/UrgencyNumberController.php
@@ -3,15 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\UrgencyCategory;
+use App\Models\UrgencyNumber;
 use Illuminate\Http\Request;
 
 class UrgencyNumberController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        // get all categories
+        $query = null;
+        // get all categories and numbers
         $categories = UrgencyCategory::all();
+        $numbers = UrgencyNumber::all();
+        // if query is present and it's not null
+        if ($request->has('query') && $request->input('query') !== 'null') {
+            // get numbers by her category id
+            $query = $request->input('query');
+            $numbers = UrgencyNumber::where('category_id', '=', $query)->get();
+        }
         // render view with categories
-        return view('urgencyNumber/index', ['categories' => $categories]);
+        return view('urgencyNumber/index', ['categories' => $categories, 'numbers' => $numbers, 'query' => $query]);
     }
 }
