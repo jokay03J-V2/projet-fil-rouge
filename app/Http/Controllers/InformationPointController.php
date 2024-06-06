7f94b30863dc2a123cb46de5af5bccc12cf3b3dd
Cannellespeculos <cjeanpierre@edenschool.fr>
2024-06-06 15:35:20 +0200

    the input is working just need to finish the select

--- app/Http/Controllers/InformationPointController.php
+++ app/Http/Controllers/InformationPointController.php
@@ -12,13 +12,23 @@ class InformationPointController extends Controller
     public function index(Request $request): View
     {
         $query = null;
+        $selectquery =null;
         $services = InformationService::all();
         $points = InformationPoint::all();
 
-        if ($request->has('query') && $request->query('query') !== 'null' ) {
-            $query = $request->input('search');
+        if ($request->has('query') && $request->query('query') !== 'null'
+            && $request->has('selectquery') && $request->query('selectquery') !== 'null') {
+            $query = $request->input('query');
             $points = InformationPoint::where('postcode', 'like',  $query . '%')->get();
+
+        } else if ($request->has('selectquery') && $request->query('selectquery') !== 'null' ) {
+            $points = InformationPoint::where('postcode', 'like',  $query . '%')->get();
+
+        } else if($request->has('query') && $request->query('query') !== 'null') {
+
         }
+
+
 
         return view('information-point', compact('points', 'services' ));
     }
