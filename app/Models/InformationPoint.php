c2230cc441d66f563090862634771916168f69c7
Cannellespeculos <cjeanpierre@edenschool.fr>
2024-06-06 15:33:15 +0200

    add manytomany to both table

--- app/Models/InformationPoint.php
+++ app/Models/InformationPoint.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class InformationPoint extends Model
 {
@@ -12,4 +13,9 @@ class InformationPoint extends Model
     use HasFactory;
 
     protected $fillable = ["name", "hourly", "departement"];
+
+    public function InformationService(): BelongsToMany
+    {
+        return $this->belongsToMany(InformationService::class);
+    }
 }
