4f325bcc42f3cf0cca3af7268243377f74874d12
Cannellespeculos <cjeanpierre@edenschool.fr>
2024-06-06 15:35:20 +0200

    the input is working just need to finish the select

--- app/Http/Controllers/InformationPointController.php
+++ app/Http/Controllers/InformationPointController.php
@@ -37,6 +37,9 @@ class InformationPointController extends Controller
         } else if($request->has('query') && $request->query('query') !== 'null') {
             $query = $request->input('query');
             $points = InformationPoint::where('postcode', 'like',  $query . '%')->get();
+
+        } else if($request->has('query') && $request->query('query') !== 'null') {
+
         }
 
 
