cbb46e89048e8be29db88a51372e7ec9114ac07b
jokay03J-V2 <jmagro@edenschool.fr>
2024-06-06 14:22:44 +0200

    update urgency numbers index

--- resources/views/urgencyNumber/index.blade.php
+++ resources/views/urgencyNumber/index.blade.php
@@ -1,14 +1,36 @@
 <x-app-layout>
+    <!-- header -->
     <x-slot name="header">
-        <h1>Rechercher un numéro d’urgence</h1>
-        <form action="{{route('page.numbers')}}" class="flex gap-1">
+        <h1 class="font-title text-4xl m-4 text-center">Rechercher un numéro d’urgence</h1>
+        <x-form action="{{route('page.numbers')}}">
             <x-select name="query" id="query">
-                <option value="null" disabled selected>Rechercher par catégorie</option>
+                <option value="null" @unless($query !== 'null') selected @endunless>Tout les catégories</option>
                 @foreach ($categories as $category)
-                    <option value="{{$category->id}}">{{$category->name}}</option>
+                    <option value="{{$category->id}}" @if ($category->id === (int) $query) selected @endif>{{$category->name}}
+                    </option>
                 @endforeach
             </x-select>
-            <x-submit-button>Send</x-submit-button>
-        </form>
+            <x-submit-button>Rechercher</x-submit-button>
+        </x-form>
     </x-slot>
+    <!-- main -->
+    <ul class="px-10 grid grid-cols-1 gap-2 mb-10 md:grid-cols-2 lg:grid-cols-4">
+        @foreach ($numbers as $number)        
+            <li class="w-full">
+                <article class="p-5 flex flex-col items-center rounded-xl border border-tonal" class="w-full">
+                    <h2 class="truncate font-title w-full text-2xl text-center text-secondary">{{$number->name}}</h2>
+                    <ul class="w-fit flex flex-col items-center mt-2 gap-2">
+                        <li class="flex items-center w-fit">
+                            <x-lucide-phone class="w-7 mr-2" />
+                            <a href="tel:{{$number->content}}" class="text-base">{{$number->content}}</a>
+                        </li>
+                        <li class="flex items-center w-fit">
+                            <x-lucide-calendar class="w-7 mr-2" />
+                            <p>{{$number->hourly}}</p>
+                        </li>
+                    </ul>
+                </article>
+            </li>
+        @endforeach
+    </ul>
 </x-app-layout>
