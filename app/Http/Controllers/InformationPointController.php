ce47d24c7bdc11ac396e488787e316b4feb7d533
Cannellespeculos <cjeanpierre@edenschool.fr>
2024-06-06 17:16:59 +0200

    add select for services and input for postcode

--- app/Http/Controllers/InformationPointController.php
+++ app/Http/Controllers/InformationPointController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\InformationPoint;
 use App\Models\InformationService;
+use App\Models\ServicePointInformation;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -19,13 +20,23 @@ class InformationPointController extends Controller
         if ($request->has('query') && $request->query('query') !== 'null'
             && $request->has('selectquery') && $request->query('selectquery') !== 'null') {
             $query = $request->input('query');
-            $points = InformationPoint::where('postcode', 'like',  $query . '%')->get();
+            $resultPoints = InformationPoint::where('postcode', 'like',  $query . '%')->get();
+            $result = $resultPoints->filter(function ($point) use ($request) {
+                return count(ServicePointInformation::where([['information_id', '=', $point->id], ['service_id', '=',(int) $request->input('selectquery')]])->get()) > 0;
+            });
+            $points = $result;
 
-        } else if ($request->has('selectquery') && $request->query('selectquery') !== 'null' ) {
-            $points = InformationPoint::where('postcode', 'like',  $query . '%')->get();
+        }
+        
+        else if ($request->has('selectquery') && $request->query('selectquery') !== 'null' ) {
+            $result = $points->filter(function ($point) use ($request) {
+                return count(ServicePointInformation::where([['information_id', '=', $point->id], ['service_id', '=',(int) $request->input('selectquery')]])->get()) > 0;
+            });
 
+            $points = $result;
         } else if($request->has('query') && $request->query('query') !== 'null') {
-
+            $query = $request->input('query');
+            $points = InformationPoint::where('postcode', 'like',  $query . '%')->get();
         }
 
 
