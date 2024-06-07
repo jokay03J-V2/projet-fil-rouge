443ffbfd8b9c36c9b0c99a5959e996d8560fc869
Cyberfox206 <bbrontesi@edenschool.fr>
2024-06-07 16:02:32 +0200

    add route home in a nav

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,7 +4,7 @@
     <x-lucide-menu class="w-14 lg:hidden z-10" id="btn_home_menu_action"></x-lucide-menu>
     <ul class="w-0 overflow-hidden pt-14 bg-white text-black h-screen right-0 top-0 absolute lg:flex lg:bg-transparent lg:w-fit lg:h-fit lg:relative lg:text-white lg:pt-0 transition-all ease-in-out duration-700"
         id="home_menu_list">
-        <x-link-nav href="#">Conseils</x-link-nav>
+        <x-link-nav href="{{route('page.advices')}}">Conseils</x-link-nav>
         <x-link-nav href="{{route('page.find_practitioners')}}">Praticien </x-link-nav>
         <x-link-nav href="{{route('page.numbers')}}">Numéro d’urgence</x-link-nav>
         <x-link-nav href="{{route('information.point')}}">Point d’informations</x-link-nav>
