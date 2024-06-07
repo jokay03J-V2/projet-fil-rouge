c8eeaddecdfb61a5d3ec9ebc54f1e070059484f3
Ugo <udauvergne@edenschool.fr>
2024-06-07 11:29:37 +0200

    correction advice

--- resources/views/advice.blade.php
+++ resources/views/advice.blade.php
@@ -1,9 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
         <h1 class="text-6xl font-title p-6 text-center mb-16">{{ $advice->name }}</h1>
-
-        </div>
-
     </x-slot>
 
     <div class="flex flex-col items-center">
@@ -15,7 +12,7 @@
             </p>
         </article>
 
-        <a href="/advices" class="md:hidden"><button class="bg-primary p-2 m-14 w-20 rounded-lg mb-32">retour</button></a>
+        <a href="{{route("page.advices")}}" class=" bg-primary p-3 m-14 rounded-lg mb-32 block md:hidden text-black ">Retour</a>
 
     </div>
 </x-app-layout>
