0342dabd1b37fe444d76d36aeb2464d29b1d2cea
Cannellespeculos <cjeanpierre@edenschool.fr>
2024-06-10 15:25:34 +0200

    add more seed

--- database/seeders/InformationPointSeeder.php
+++ database/seeders/InformationPointSeeder.php
@@ -16,9 +16,9 @@ class InformationPointSeeder extends Seeder
     public function run(): void
     {
 //        seed information_point table
-        $points = InformationPoint::factory(5)->create();
+        $points = InformationPoint::factory(20)->create();
 //        seed information_service table
-        $services = InformationService::factory(5)->create();
+        $services = InformationService::factory(10)->create();
 //        seed random ids to information_service-information
         $points->each(function ($point) use ($services) {
             $services->each(function ($service) use ($point) {
