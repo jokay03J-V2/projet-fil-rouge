a83b923d717f08eec4a6cddd49f81e62812d5589
jokay03J-V2 <jmagro@edenschool.fr>
2024-06-10 11:32:54 +0200

    add img

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,7 @@
 @vite(['resources/js/home_menu.js'])
 <nav class="w-full flex justify-between items-center p-2 relative">
-    <a href="{{route('page.home')}}" class="text-5xl ml-4 font-title">{{config('app.name', 'title')}}</a>
+    <a href="{{route('page.home')}}" class="text-5xl ml-4 font-title"><img src="./img_folder/logo.png" alt="logo"
+            class="h-16"></a>
     <x-lucide-menu class="w-14 lg:hidden z-10" id="btn_home_menu_action"></x-lucide-menu>
     <ul class="w-0 overflow-hidden pt-14 bg-white text-black h-screen right-0 top-0 absolute lg:flex lg:bg-transparent lg:w-fit lg:h-fit lg:relative lg:text-white lg:pt-0 transition-all ease-in-out duration-700"
         id="home_menu_list">
