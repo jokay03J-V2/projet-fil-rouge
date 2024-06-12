47b7adb97b894fb0a471acedc34f65efa6924298
jokay03J-V2 <jmagro@edenschool.fr>
2024-06-12 11:52:37 +0200

    add class tailwidn merge

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,5 +1,5 @@
 @props(['action', 'method' => 'GET'])
 
-<form action="{{$action}}" method="{{$method}}" {{ $attributes->merge(['class' => 'flex flex-col gap-1 mb-5 w-9/12 pt-2 md:flex-row md:justify-center mt-24']) }}>
+<form action="{{$action}}" method="{{$method}}" {{$attributes->twMerge('flex flex-col gap-1 mb-5 w-9/12 pt-2 md:flex-row md:justify-center mt-24')}}>
     {{$slot}}
 </form>
