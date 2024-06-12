684ba354a12237388b69497a07eaa846fda369e6
jokay03J-V2 <jmagro@edenschool.fr>
2024-06-12 11:51:49 +0200

    add register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
 
 class UserController extends Controller
 {
@@ -20,7 +24,7 @@ class UserController extends Controller
             $request->authenticate();
             $request->session()->regenerate();
             // call login request and generate a session
-            return redirect()->intended('/'); 
+            return redirect()->intended('/');
         } catch (\Throwable $th) {
             return back()->withErrors([
                 'email' => 'Votre mot de passe ou votre adresse e-mail est erroné(e). Essayez de les saisir à nouveau.',
@@ -35,5 +39,25 @@ class UserController extends Controller
         $request->session()->regenerateToken();
         return redirect('/');
         // logout and return to login view
+    }
+
+    public function showRegister(): View
+    {
+        // render register view
+        return view('admin/register/new');
+    }
+
+    public function register(RegisterRequest $request): RedirectResponse|View
+    {
+        // handle error
+        try {
+            // create user into the db
+            User::create($request->all());
+        } catch (\Throwable $th) {
+            // render view to display errors
+            return view('admin/register/new');
+        }
+        // redirect to admin home
+        return redirect(route('admin.home'));
     }
 }
