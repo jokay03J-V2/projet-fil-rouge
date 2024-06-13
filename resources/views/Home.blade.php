ae8b60058d9e0cd311522ef78d866697562cea5f
Cyberfox206 <bbrontesi@edenschool.fr>
2024-06-13 11:32:09 +0200

    fixt style of home page

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -13,15 +13,15 @@
    <!------------------------------------------------------------------------------------------------------>
    <!-- main content -->
    <!------------------------------------------------------------------------------------------------------>
-   <section class="flex flex-col justify-center items-center gap-10 lg:gap-14 w-11/12 mx-auto my-20">
-      <h2 class="lg:self-start font-title text-4xl mt-14">Fonctionnalitées</h2>
+   <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-10 lg:gap-14 w-11/12 mx-auto my-20">
+      <h2 class="lg:self-start font-title text-4xl mt-14 md:col-span-2 lg:col-span-1">Fonctionnalitées</h2>
 
       <!-- Home card -->
-      <a href="{{ route('information.point') }}">
+      <a href="{{ route('information.point') }}" class="md:h-full lg:h-full">
          <article
-            class="flex flex-col lg:flex-row items-center lg:items-start justify-center w-full rounded-2xl border-2 border-tonal">
+            class="flex flex-col lg:flex-row items-center lg:items-start lg:h-full w-full h-full rounded-2xl border-2 border-tonal">
             <img src="./img_folder/information_point.jpg" alt="/"
-               class="w-full lg:w-1/4 h-64 md:h-80 object-cover rounded-t-xl lg:rounded-l-xl lg:rounded-r-none">
+               class="w-full lg:w-1/4 h-40 md:h-80 object-cover rounded-t-xl lg:rounded-l-xl lg:h-80 lg:rounded-r-none">
             <hgroup class="w-10/12 p-10 text-center lg:text-start">
                <h3 class="text-secondary text-3xl font-title m-2">Point d'informations</h3>
                <p class="m-2">
@@ -35,11 +35,11 @@
       </a>
 
       <!-- Conseils card -->
-      <a href="{{ route('page.advices') }}">
+      <a href="{{ route('page.advices') }}" class="md:h-full lg:h-full">
          <article
             class="flex flex-col lg:flex-row items-center lg:items-start justify-center w-full rounded-2xl border-2 border-tonal">
             <img src="./img_folder/conseil.jpg" alt="/"
-               class="w-full lg:w-1/4 h-64 md:h-80 object-cover rounded-t-xl lg:rounded-l-xl lg:rounded-r-none">
+               class="w-full lg:w-1/4 h-40 md:h-80 object-cover rounded-t-xl lg:rounded-l-xl lg:h-80 lg:rounded-r-none">
             <hgroup class="w-10/12 p-10 text-center lg:text-start">
                <h3 class="text-secondary text-3xl font-title m-2">Conseils</h3>
                <p class="m-2">
@@ -53,11 +53,11 @@
       </a>
 
       <!-- Praticiens card -->
-      <a href="{{ route('page.find_practitioners') }}">
+      <a href="{{ route('page.find_practitioners') }}" class="h-full">
          <article
             class="flex flex-col lg:flex-row items-center lg:items-start justify-center w-full rounded-2xl border-2 border-tonal">
             <img src="./img_folder/practitioners.jpg" alt="/"
-               class="w-full lg:w-1/4 h-60 md:h-80 object-cover rounded-t-xl lg:rounded-l-xl lg:rounded-r-none">
+               class="w-full lg:w-1/4 h-40 md:h-80 object-cover rounded-t-xl lg:rounded-l-xl lg:h-80 lg:rounded-r-none">
             <hgroup class="w-10/12 p-10 text-center lg:text-start">
                <h3 class="text-secondary text-3xl font-title m-2">Praticiens</h3>
                <p class="m-2">
@@ -74,7 +74,7 @@
          <article
             class="flex flex-col lg:flex-row items-center lg:items-start justify-center w-full rounded-2xl border-2 border-tonal">
             <img src="./img_folder/emergency_number.jpg" alt="/"
-               class="w-full lg:w-1/4 h-64 md:h-80 object-cover rounded-t-xl lg:rounded-l-xl lg:rounded-r-none">
+               class="w-full lg:w-1/4 h-40 md:h-80 object-cover rounded-t-xl lg:rounded-l-xl lg:h-full lg:rounded-r-none">
             <hgroup class="w-10/12 p-10 text-center lg:text-start">
                <h3 class="text-secondary text-3xl font-title m-2">Numéro d’urgence</h3>
                <p class="m-2">
